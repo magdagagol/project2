cbfdc5b5ef4c983f0031e16081683ff41d6a7045
magdagagol <gagolmagdalena@yahoo.com>
2022-09-14 17:51:04 +0200

    feat: create main component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TasksTableComponent } from './tasks-table/tasks-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TasksTableComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TasksTableComponent } from './tasks-table/tasks-table.component';
+import { MatCardModule } from '@angular/material/card'; 
+import { MatTableModule } from '@angular/material/table'; 
+import { MatInputModule } from '@angular/material/input'; 
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatTableModule,
+    MatInputModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
